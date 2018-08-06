40f4d2ecd11204b198afb41d4ab95fbc3a6ca90b
Alex Marshall <alex.marshall@bristol.ac.uk>
2018-08-06 09:56:45 +0100

    replaced Flatten

--- GAN_BC.py
+++ GAN_BC.py
@@ -1,6 +1,6 @@
 import numpy as np
 from keras.datasets import mnist
-from keras.layers import Input, Dense, Reshape, Dropout
+from keras.layers import Input, Dense, Flatten, Reshape, Dropout
 from keras.layers import BatchNormalization
 from keras.layers.advanced_activations import LeakyReLU
 from keras.models import Sequential
